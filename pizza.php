e15ca608aa68328c2ffc6597a9497c6bbdfa31cf
Lisa-Maria Förster <Lisamari9500@gmail.com>
2024-06-08 12:14:46 +0200

    add pizza.php.

---
+++ pizza.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/style.css">
+  <title>BACIO</title>
+</head>
+<body>
+
+<header>
+
+
+
+
+
+</header>
+
+
+<main>
+
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<section id="pizza__sortiment">
+        <div class="container">
+          <div class="row">
+            <h2 class="text-grey pb-5 d-flex justify-content-center">Unsere Zutaten
+            </h2>
+            <div class="col-md-4 text-center d-flex justify-content-center">
+            <div class="card custom-radius bg-red m-2">
+                <img class="card-img-top custom-radius" src="img/p-pizzas.jpg" alt="Card image cap">
+                <div class="card-body">
+                  <h5 class="card-title d-flex justify-content-start">Margerita</h5>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-4 text-center d-flex justify-content-center">
+            <div class="card custom-radius bg-red m-2">
+              <img class="card-img-top custom-radius" src="img/p-pizzas.jpg" alt="Card image cap">
+              <div class="card-body">
+                <h5 class="card-title d-flex justify-content-start">Margerita</h5>
+              </div>
+            </div>
+            </div>
+            <div class="col-md-4 text-center d-flex justify-content-center">
+            <div class="card custom-radius bg-red m-2">
+              <img class="card-img-top custom-radius" src="img/p-pizzas.jpg" alt="Card image cap">
+              <div class="card-body">
+                <h5 class="card-title d-flex justify-content-start">Margerita</h5>
+              </div>
+            </div>
+            </div>
+          </div>
+        </div>
+        <div class="container mt-5">
+          <div class="row">
+            <div class="col-md-4 text-center d-flex justify-content-center">
+            <div class="card custom-radius bg-red m-2">
+                <img class="card-img-top custom-radius" src="img/p-pizzas.jpg" alt="Card image cap">
+                <div class="card-body">
+                  <h5 class="card-title d-flex justify-content-start">Margerita</h5>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-4 text-center d-flex justify-content-center">
+            <div class="card custom-radius bg-red m-2">
+              <img class="card-img-top custom-radius" src="img/p-pizzas.jpg" alt="Card image cap">
+              <div class="card-body">
+                <h5 class="card-title d-flex justify-content-start">Margerita</h5>
+              </div>
+            </div>
+            </div>
+            <div class="col-md-4 text-center d-flex justify-content-center">
+            <div class="card custom-radius bg-red m-2">
+              <img class="card-img-top custom-radius" src="img/p-pizzas.jpg" alt="Card image cap">
+              <div class="card-body">
+                <h5 class="card-title d-flex justify-content-start">Margerita</h5>
+              </div>
+            </div>
+            </div>
+          </div>
+        </div>
+        <div class="container mt-5">
+          <div class="row">
+            <div class="col-md-4 text-center d-flex justify-content-center">
+            <div class="card custom-radius bg-red m-2">
+                <img class="card-img-top custom-radius" src="img/p-pizzas.jpg" alt="Card image cap">
+                <div class="card-body">
+                  <h5 class="card-title d-flex justify-content-start">Margerita</h5>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-4 text-center d-flex justify-content-center">
+            <div class="card custom-radius bg-red m-2">
+              <img class="card-img-top custom-radius" src="img/p-pizzas.jpg" alt="Card image cap">
+              <div class="card-body">
+                <h5 class="card-title d-flex justify-content-start">Margerita</h5>
+              </div>
+            </div>
+            </div>
+            <div class="col-md-4 text-center d-flex justify-content-center">
+            <div class="card custom-radius bg-red m-2">
+              <img class="card-img-top custom-radius" src="img/p-pizzas.jpg" alt="Card image cap">
+              <div class="card-body">
+                <h5 class="card-title d-flex justify-content-start">Margerita</h5>
+              </div>
+            </div>
+            </div>
+          </div>
+        </div>
+</section>
+<br>
+<br>
+<br><br>
+<br>
+<br>
+
+</main>
+  
+<footer>
+
+
+
+
+</footer>
+
+
+</body>
+</html>
