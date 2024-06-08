b73c7236be50d15ba6154a64dfc4621784c98f27
Lisa-Maria Förster <Lisamari9500@gmail.com>
2024-06-08 23:28:44 +0200

    updatet footer.

--- footer.php
+++ footer.php
@@ -1,25 +1,31 @@
-
-<footer class="bg-red pt-5 pb-5 mt-5">
-    <div class="container">
-            <div class="row d-flex justify-content-around">
-            <div class="col-sm-3 d-flex flex-column text-center justify-content-center">
-                    <a href=""><img class="logo-footer" src="img/bacio-logo-white.png" alt=""></a>
+<footer>
+<div class="container">
+        <div class="row">
+            <div class="col-6 col-md-3 col-lg-3">
+                <div class="text-white bg1">
+                <a href=""><img class="logo-footer" src="img/bacio-logo-white.png" alt=""></a>
                 </div>
-                <div class="col-sm-3 d-flex flex-column text-center justify-content-center">
+            </div>
+            <div class="col-6 col-md-3 col-lg-3">
+                <div class="text-white bg2">
                 <p class="text-white">Adress</p>
                 <p class="text-white">tele</p>
                 <p class="text-white">email</p>
                 </div>
-                <div class="col-sm-3 d-flex flex-column text-center justify-content-center">
+            </div>
+            <div class="col-6 col-md-3 col-lg-3">
+                <div class="text-white bg3">
                 <a href="">Datenschutz</a>
                 <a href="">Impressum</a>
                 <a href="">AGB</a>
                 </div>
-                <div class="col-sm-3 d-flex flex-column text-center justify-content-center">
+            </div>
+            <div class="col-6 col-md-3 col-lg-3">
+                <div class="text-white bg4">
                 <p class="text-white">facebook</p>
                 <p class="text-white">instagram</p>
-                <p class="text-white">erjnferiuf</p>
                 </div>
             </div>
-    </div>
+        </div>
+</div>
 </footer>
