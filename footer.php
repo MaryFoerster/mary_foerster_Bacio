6812b933883c298033f5349478ce9ccc3ce6249e
Lisa-Maria Förster <Lisamari9500@gmail.com>
2024-06-08 21:16:56 +0200

    updatet footer.

--- footer.php
+++ footer.php
@@ -3,21 +3,21 @@
     <div class="container">
             <div class="row d-flex justify-content-around">
             <div class="col-sm-3 d-flex flex-column text-center justify-content-center">
-                  <p class="text-white">erjnferiuf</p>
+                    <a href=""><img class="logo-footer" src="img/bacio-logo-white.png" alt=""></a>
                 </div>
                 <div class="col-sm-3 d-flex flex-column text-center justify-content-center">
-                <p class="text-white">erjnferiuf</p>
-                <p class="text-white">erjnferiuf</p>
-                <p class="text-white">erjnferiuf</p>
+                <p class="text-white">Adress</p>
+                <p class="text-white">tele</p>
+                <p class="text-white">email</p>
                 </div>
                 <div class="col-sm-3 d-flex flex-column text-center justify-content-center">
-                <p class="text-white">erjnferiuf</p>
-                <p class="text-white">erjnferiuf</p>
-                <p class="text-white">erjnferiuf</p>
+                <a href="">Datenschutz</a>
+                <a href="">Impressum</a>
+                <a href="">AGB</a>
                 </div>
                 <div class="col-sm-3 d-flex flex-column text-center justify-content-center">
-                <p class="text-white">erjnferiuf</p>
-                <p class="text-white">erjnferiuf</p>
+                <p class="text-white">facebook</p>
+                <p class="text-white">instagram</p>
                 <p class="text-white">erjnferiuf</p>
                 </div>
             </div>
