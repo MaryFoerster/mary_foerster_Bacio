67ad5a93641b208ec9a5653f38eb530628a96ccc
Lisa-Maria Förster <Lisamari9500@gmail.com>
2024-06-08 20:43:57 +0200

    add pages.

---
+++ artikel.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/style.css">
+  <title>Artikel</title>
+</head>
+<body>
+
+<header>
+<?php include("nav.php") ?>
+
+</header>
+
+
+<main>
+
+<section id="index__beschreibung">
+        <div class="container mt-5 mb-5">
+          <div class="row p-4">
+            <div class="col-lg-6">
+            <h2>Beschreibung</h2>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum praesentium libero impedit enim asperiores sunt maxime tenetur eius fuga corrupti odit dolor quas delectus ut molestias ex, est rerum incidunt consequatur earum dignissimos reprehenderit dolorum quam unde. Fuga ex, adipisci architecto, laudantium deleniti nobis accusantium iure animi laboriosam minus praesentium eaque labore eligendi dolorem eum dolores porro blanditiis nam, totam autem delectus perspiciatis. Placeat quas repellendus odio iusto autem quia deserunt totam voluptatem.</p>
+            </div>
+            <div class="col-lg-6 d-flex justify-content-center">
+           <img class="img-fluid custom-radius" src="img/platzhalter.jpg" alt="">
+            </div>
+          </div>
+          <div class="row p-4">
+            <div class="col-lg-6">
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum praesentium libero impedit enim asperiores sunt maxime tenetur eius fuga corrupti odit dolor quas delectus ut molestias ex, est rerum incidunt consequatur earum dignissimos reprehenderit dolorum quam unde. Fuga ex, adipisci architecto, laudantium deleniti nobis accusantium iure animi laboriosam minus praesentium eaque labore eligendi dolorem eum dolores porro blanditiis nam, totam autem delectus perspiciatis. Placeat quas repellendus odio iusto autem quia deserunt totam voluptatem.</p>
+            </div>
+            <div class="col-lg-6">
+             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum praesentium libero impedit enim asperiores sunt maxime tenetur eius fuga corrupti odit dolor quas delectus ut molestias ex, est rerum incidunt consequatur earum dignissimos reprehenderit dolorum quam unde. Fuga ex, adipisci architecto.</p>
+             <img class="img-fluid custom-radius" src="img/icon-tomate.png" alt="">
+            </div>
+          </div>
+        </div>
+</section>
+
+</main>
+
+<?php include("footer.php") ?>
+
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
