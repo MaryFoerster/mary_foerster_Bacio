9e0cf6f2e3ecfb2fecf554da5f7ee96dda0213e2
Lisa-Maria Förster <Lisamari9500@gmail.com>
2024-06-15 12:13:11 +0200

    updatet footer.

--- footer.php
+++ footer.php
@@ -33,31 +33,34 @@
 
       <div class="row">
 
-        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+        <div class="col-lg-3 col-md-6 mb-4 mb-md-0 mt-lg-1">
           <ul class="list-unstyled mb-0">
             <li>
                 <a href="">  <img class="logo-footer" src="img/bacio-logo-white.png" alt=""></a>
             </li>
+            <li>
+                <img class="logo-footer pt-5" src="img/footer-img.png" alt="">
+            </li>
           </ul>
         </div>
 
-        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+        <div class="col-lg-3 col-md-6 mb-4 mb-md-0 mt-lg-1">
           <h5 class="text-uppercase text-white">Kontakt</h5>
 
           <ul class="list-unstyled mb-0">
          <li class="pt-4">
-              <img class="img-footer" src="img/phone.png" alt=""><p class="text-white">+43 664 4132694</p>
+              <img class="img-footer" src="img/location-white.png" alt=""><p class="text-white">Hofgasse 4, 6020 Innsbruck</p>
             </li>
             <li>
-             <img class="img-footer" src="img/phone.png" alt=""><p class="text-white">+43 664 4132694</p>
+             <img class="img-footer" src="img/phone-white.png" alt=""><p class="text-white">+43 664 4132694</p>
             </li>
             <li>
-              <img class="img-footer" src="img/phone.png" alt=""><p class="text-white">+43 664 4132694</p>
+              <img class="img-footer" src="img/email-white.png" alt=""><p class="text-white">www.pizzabacio.com</p>
             </li>
           </ul>
         </div>
 
-        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+        <div class="col-lg-3 col-md-6 mb-4 mb-md-0 mt-lg-1 mt-md-4">
         <h5 class="text-uppercase text-white">links</h5>
           <ul class="list-unstyled">
           <li class="pt-4">
@@ -71,7 +74,7 @@
             </li>
           </ul>
         </div>
-        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+        <div class="col-lg-3 col-md-6 mb-4 mb-md-0 mt-lg-1 mt-md-4">
           <h5 class="text-uppercase text-white">Social Media</h5>
 
           <ul class="list-unstyled mb-0">
@@ -86,7 +89,7 @@
       </div>
     </section>
   </div>
-  <div class="text-center text-white p-3">
-    © 2024 Bacio
+  <div class="text-center p-3 bg-white mt-2">
+   <p class="text-red">© 2024 Bacio</p>
   </div>
 </footer>
