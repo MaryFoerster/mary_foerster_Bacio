e367187bba495e9cea95cf1a7d96c3c38a1cc7f9
Lisa-Maria Förster <Lisamari9500@gmail.com>
2024-06-16 11:43:49 +0200

    updatet über uns.

--- ueberUns.php
+++ ueberUns.php
@@ -37,6 +37,46 @@
         </div>
 </section>
 
+
+<section id="index__zutaten">
+        <div class="pb-5 pt-5">
+          <div class="container">
+          <div class="row">
+            <h2 class="text-grey pb-5 d-flex justify-content-center">Holzsorten
+            </h2>
+            <div class="col-12 text-center d-flex justify-content-center align-items-center flex-column">
+            <p class="custom-beschreibung">Lecker wie beim Italiener
+            Pizza BACIO ist die erste Tiefkühlpizza, die nicht schmeckt wie eine Tiefkühlpizza. Dafür verwendet man nur hochwertigste Zutaten und bäckt die Pizza bei über 400°C im Steinofen vor. Dabei kommen auch Holzsorten zum Einsatz, die den Geschmack beeinflussen.</p>
+            </div>
+            <div class="col-md-4 text-center">
+            <img class="img-fluid custom-radius" src="img/basilikum.png" alt="">
+              <p class="text-grey"><strong>Eiche</strong>
+              Pizza Prosciutto e Funghi
+              Pizza Salame
+
+                Eiche ist eine Holzart, die für ihren starken und rauchigen Geschmack bekannt ist. Beim Backen von Pizza auf Eichenholz erhält die Pizza einen intensiven und robusten Geschmack, der gut zu würzigen Belägen wie Salami oder scharfen Peperoni passt.</p>
+            </div>
+            <div class="col-md-4 text-center">
+            <img class="img-fluid custom-radius" src="img/mozzarella.png" alt="">
+              <p class="text-grey"><strong>Buchenholz</strong> <br>
+                  Pizza Margherita<br>
+                  Pizza Prosciutto e Ananas<br>
+                  Pizza Quattro Formaggi<br>
+                  Pizza Spinaci e Ricotta<br>
+                Buchenholz ist eine beliebte Wahl zum Backen von Pizza, da es einen milden, süßen Rauchgeschmack verleiht. Es ergänzt eine Vielzahl von Belägen gut und ist ideal für klassische Pizzen wie Margherita oder Pizza mit Schinken und Pilzen.</p>
+            </div>
+            <div class="col-md-4 text-center">
+            <img class="img-fluid custom-radius" src="img/tomate.png" alt="">
+              <p class="text-grey"><strong>Kirschholz</strong>
+              Pizza Tonno e Cipolla
+              Pizza Salame Piccante
+                Kirschholz bietet einen süßlichen und fruchtigen Rauchgeschmack, der der Pizza eine einzigartige Note verleiht. Es eignet sich besonders gut für Pizzen mit fruchtigen oder leicht süßen Belägen wie Prosciutto und Feigen oder Pizza mit Ziegenkäse und Honig.</p>
+            </div>
+          </div>
+          </div>
+        </div>
+</section>
+
 </main>
 
 <?php include("footer.php") ?>
