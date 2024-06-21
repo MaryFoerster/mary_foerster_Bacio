f1b37a065e90edaf61db94c5194f7e7e0b24dcbb
Lisa-Maria Förster <Lisamari9500@gmail.com>
2024-06-21 21:01:47 +0200

    optimized nav.

--- nav.php
+++ nav.php
@@ -1,15 +1,15 @@
 <nav class="navbar navbar-expand-md pt-3 pb-3 sticky-top bg-none">
     <div class="container">
-      <a class="navbar-brand bg-red p-2 custom-radius" href="index.php"><img id="bacio-logo" class="logo-nav" src="img/bacio-logo-white.png"/></a>
+      <a class="navbar-brand bg-red p-2 custom-radius" href="index.php"><img id="bacio-logo" class="logo-nav" src="img/bacio-logo-white.png" alt="bacio-logo" title="bacio-logo"/></a>
       <button class="navbar-toggler bg-red custom-radius" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-        <span><img class="img-nav" src="img/burger-white.png" alt=""></span>
+        <span><img class="img-nav" src="img/burger-white.png" alt="burger-menu" title="burger-menu"></span>
       </button>
       <div class="collapse navbar-collapse justify-content-end pt-3 pt-md-1 pb-md-0" id="navbarNavAltMarkup">
         <div class="navbar-nav bg-red custom-radius">
-          <a class="nav-link active me-2 text-white" href="pizza.php">Pizza</a>
-          <a class="nav-link active me-2 text-white" href="produkte.php">Produkte</a>
-          <a class="nav-link active me-2 text-white" href="ueberUns.php">Über uns</a>
-          <a class="nav-link active me-2 text-white" href="kontakt.php">Kontakt</a>
+          <a class="nav-link active me-2 text-white" href="pizza.php" alt="Pizza-nav-link" title="Pizza-nav-link">Pizza</a>
+          <a class="nav-link active me-2 text-white" href="produkte.php" alt="Produkte-nav-link" title="Produkte-nav-link">Produkte</a>
+          <a class="nav-link active me-2 text-white" href="ueberUns.php" alt="ueberUns-nav-link" title="ueberUns-nav-link">Über uns</a>
+          <a class="nav-link active me-2 text-white" href="kontakt.php" alt="Kontakt-nav-link" title="Kontakt-nav-link">Kontakt</a>
         </div>
       </div>
     </div>
