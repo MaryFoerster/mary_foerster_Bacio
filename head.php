56927ec1c2e61cd3e3100c8d7b03a1f73f15a056
Lisa-Maria Förster <Lisamari9500@gmail.com>
2024-06-21 20:54:28 +0200

    add Meta-Description.

--- head.php
+++ head.php
@@ -1,5 +1,6 @@
 <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="Willkommen bei BACIO – Die beste Tiefkühlpizza mit authentischem italienischem Geschmack. Handgemachter Teig, hochwertige Zutaten, im Steinofen vorgebacken. Erleben Sie den Unterschied!">
   <link rel="stylesheet" href="css/bootstrap.min.css">
   <link rel="stylesheet" href="css/style.css">
 <link rel="apple-touch-icon" sizes="180x180" href="favi/apple-touch-icon.png">
