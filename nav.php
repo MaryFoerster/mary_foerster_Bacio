c56c6be9ce5e96b3a8115dce01bc5e3946ab6294
Lisa-Maria Förster <Lisamari9500@gmail.com>
2024-06-21 08:24:29 +0200

    finished nav.

--- nav.php
+++ nav.php
@@ -1,8 +1,8 @@
 <nav class="navbar navbar-expand-md pt-3 pb-3 sticky-top bg-none">
     <div class="container">
       <a class="navbar-brand bg-red p-2 custom-radius" href="index.php"><img id="bacio-logo" class="logo-nav" src="img/bacio-logo-white.png"/></a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-        <span><img class="img-nav" src="img/mozzarella.png" alt=""></span>
+      <button class="navbar-toggler bg-red custom-radius" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span><img class="img-nav" src="img/burger-white.png" alt=""></span>
       </button>
       <div class="collapse navbar-collapse justify-content-end pt-3 pt-md-1 pb-md-0" id="navbarNavAltMarkup">
         <div class="navbar-nav bg-red custom-radius">
